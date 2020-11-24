e25562e3b62e81ea66c02013e27c958e2359d2a4
rusdeveloper23 <gusev-a@mail.ru>
2020-11-24 21:02:13 +0300

    add travis

--- sfinae.cpp
+++ sfinae.cpp
@@ -14,11 +14,6 @@ template <typename T> struct is_stl_container {
   static constexpr bool const value = is_stl_container_impl::is_stl_container<std::decay_t<T>>::value;
 };
 
-template<typename T, typename std::enable_if<std::is_integral<T>::value, int>::type = 0>
-void print_ip(T&& ip) {
-	std::cout << +ip << std::endl;
-}
-
 template<class...> struct disjunction : std::false_type { };
 template<class B1> struct disjunction<B1> : B1 { };
 template<class B1, class... Bn>
@@ -34,54 +29,41 @@ struct is_string
         > {
 };
 
-/*!
-Находит сумму двух чисел
-\param a,b Складываемые числа
-\return Сумму двух чисел, переданных в качестве аргументов
-*/
-double sum(const double a, const double b);
-
 template<typename T,
          typename std::enable_if<std::is_integral<T>::value, int>::type = 0>
-void foo(T number)
+void print_ip(T number)
 {
-	std::cout << ((number >> (8*sizeof(T))) & 0xff);
+	std::cout << ((number>>(8*(sizeof(T)-1))) & 0xff);
 	for(int i = (sizeof(T) - 1); i >= 0; i--)
-    	std::cout << "." << ((number >> (8*i)) & 0xff) ;
+    	std::cout << "." << ((number>>(8*i)) & 0xff) ;
 	std::cout << std::endl;
 }
 
 template<typename T, typename std::enable_if<is_string<T>::value, int>::type = 0>
-void foo(T str) {
+void print_ip(T str) {
 	std::cout << str << std::endl;
 }
 
 template<typename T, typename std::enable_if<is_stl_container<T>::value, int>::type = 0>
-void foo(T container) {
+void print_ip(T container) {
 	for(const auto& element: container)
 		std::cout << element << ".";
 	std::cout << std::endl;
 }
 
-class Parent {
-public:
-	Parent();
-	~Parent();
-};
-
 int main() {
 	char c = -1;
-	foo(c);
+	print_ip(c);
 	short s = 0;
-	foo(s);
+	print_ip(s);
 	int i = 2130706433;
-	foo(i);
+	print_ip(i);
 	long long l = 8875824491850138409;
-	foo(l);
-	foo(std::string("1.1.1.1"));
+	print_ip(l);
+	print_ip(std::string("1.1.1.1"));
 	std::vector<int> v {1, 2, 3, 4};
-	foo(v);
+	print_ip(v);
 	std::list<int> l2 {1, 2, 3, 4, 5};
-	foo(l2);
+	print_ip(l2);
 	return 0;
 }
