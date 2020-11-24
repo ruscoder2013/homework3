1434311e0c2c82ebd31eb7df7b14ef83cc1d2e36
rusdeveloper23 <gusev-a@mail.ru>
2020-11-24 20:01:06 +0300

    add doxygen

---
+++ html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['homework3',['homework3',['../md_README.html',1,'']]]
+];
--- sfinae.cpp
+++ sfinae.cpp
@@ -10,74 +10,78 @@ namespace is_stl_container_impl{
   template <typename... Args> struct is_stl_container<std::list              <Args...>>:std::true_type{};
 }
 
-//type trait to utilize the implementation type traits as well as decay the type
 template <typename T> struct is_stl_container {
   static constexpr bool const value = is_stl_container_impl::is_stl_container<std::decay_t<T>>::value;
 };
 
-template<typename T, 
-		typename Fake = 
-			typename std::enable_if<!is_stl_container<T>::value, void>::type
-	>
-void print_ip(T&& container) {
-	std::cout << "container" << std::endl;
-}
-
-
-template<typename T, 
-		typename Fake = 
-			typename std::enable_if<!std::is_integral<T>::value, void>::type
-	>
+template<typename T, typename std::enable_if<std::is_integral<T>::value, int>::type = 0>
 void print_ip(T&& ip) {
 	std::cout << +ip << std::endl;
 }
 
-// template <typename T>
-// struct is_string
-//     : public mpl::or_< // is "or_" included in the C++11 library?
-//         std::is_same<       char *, typename std::decay< T >::type >,
-//         std::is_same< const char *, typename std::decay< T >::type >
-//      > {};
+template<class...> struct disjunction : std::false_type { };
+template<class B1> struct disjunction<B1> : B1 { };
+template<class B1, class... Bn>
+struct disjunction<B1, Bn...> 
+    : std::conditional_t<bool(B1::value), B1, disjunction<Bn...>>  { };
 
+template<typename T>
+struct is_string
+        : public disjunction<
+                std::is_same<char *, typename std::decay<T>::type>,
+                std::is_same<const char *, typename std::decay<T>::type>,
+                std::is_same<std::string, typename std::decay<T>::type>
+        > {
+};
 
-void print_ip(std::string str) {
-	std::cout << "str" << std::endl;
+/*!
+Находит сумму двух чисел
+\param a,b Складываемые числа
+\return Сумму двух чисел, переданных в качестве аргументов
+*/
+double sum(const double a, const double b);
+
+template<typename T,
+         typename std::enable_if<std::is_integral<T>::value, int>::type = 0>
+void foo(T number)
+{
+	std::cout << ((number >> (8*sizeof(T))) & 0xff);
+	for(int i = (sizeof(T) - 1); i >= 0; i--)
+    	std::cout << "." << ((number >> (8*i)) & 0xff) ;
+	std::cout << std::endl;
 }
 
+template<typename T, typename std::enable_if<is_string<T>::value, int>::type = 0>
+void foo(T str) {
+	std::cout << str << std::endl;
+}
 
-// template<typename T>
-// void print_ip(std::vector<T> v) {
-// 	std::cout << "vector" << std::endl;
-// }
+template<typename T, typename std::enable_if<is_stl_container<T>::value, int>::type = 0>
+void foo(T container) {
+	for(const auto& element: container)
+		std::cout << element << ".";
+	std::cout << std::endl;
+}
 
-// template<typename T>
-// void print_ip(std::list<T> l) {
-// 	std::cout << "list" << std::endl;
-// }
-
-
+class Parent {
+public:
+	Parent();
+	~Parent();
+};
 
 int main() {
-
-	// std::cout << "hw" << std::endl;
-	// SomeStruct first("Hello, World!");
-	// SomeStruct second = first;
-
 	char c = -1;
-	print_ip(c);
+	foo(c);
 	short s = 0;
-	print_ip(s);
+	foo(s);
 	int i = 2130706433;
-	print_ip(i);
+	foo(i);
 	long long l = 8875824491850138409;
-	print_ip(l);
-	print_ip(std::string("hello world"));
+	foo(l);
+	foo(std::string("1.1.1.1"));
 	std::vector<int> v {1, 2, 3, 4};
-	print_ip(v);
+	foo(v);
 	std::list<int> l2 {1, 2, 3, 4, 5};
-	print_ip(l2);
-
-	std::cout << char(-1) << std::endl;
-
+	foo(l2);
 	return 0;
 }
