c09d32005547dee01e7ae3845392fd6959104daa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-25 01:49:54 +0000

    Deploy ruscoder2013/homework3 to github.com/ruscoder2013/homework3.git:gh-pages

---
+++ html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "dhip",
+  1: "dip",
+  2: "h"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Pages"
+};
+
